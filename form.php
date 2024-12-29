0b40fae3704531a973368446054a6e8376544afd
Nishu Ranjit <nishuranjit55@gmail.com>
2024-12-29 14:42:26 +0545

    INitial commit

---
+++ form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+
+<form action="form.php" method="POST">
+
+Email: <input name="login_email" type="email"/>
+Password: <input name="login_password" type="password"/>
+
+<button type="submit">Submit</button>
+
+</form>
+
+</body>
+</html>
+
+<?php
+if (isset($_POST['login_email'])) {
+    print_r($_POST);
+}
+// if (isset($_GET)) {
+//     print_r($_GET);
+// }
+?>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+
+<?php
+// Printing string
+// echo "Kagati";
+
+// Variable declaration 
+$x = 20;
+// echo $x;
+
+// Array
+$arr = [1, 2, 3, 4, 5, 6];
+// print_r($arr); // Way 1
+// var_dump($arr);
+
+// NEsted array
+$nest_arr = [[1, 2, 3], [4, 5, 6]];
+// echo "<pre>";
+// print_r($nest_arr);
+
+// echo $arr[2];
+// echo $nest_arr[1][2];
+
+// function
+
+function FunctionName() {
+    echo 'hello idiot';
+}
+
+FunctionName();
+
+?>
+
+</body>
+</html>
